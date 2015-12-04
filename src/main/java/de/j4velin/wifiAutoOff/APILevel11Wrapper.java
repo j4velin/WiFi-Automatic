94fcb733b5f516c3cd59460cabe5728f6c1d402c
j4velin <j4velin@gmail.com>
2015-12-04 22:07:05 +0100

    fixed nullpointerexception

--- src/main/java/de/j4velin/wifiAutoOff/APILevel11Wrapper.java
+++ src/main/java/de/j4velin/wifiAutoOff/APILevel11Wrapper.java
@@ -29,23 +29,22 @@ import android.widget.NumberPicker;
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 abstract class APILevel11Wrapper {
 
-	static void showNumberPicker(final Context c, final SharedPreferences prefs, final Preference p, final int summary,
-			final int min, final int max, final String title, final String setting, final int def, final boolean changeTitle) {
-		final NumberPicker np = new NumberPicker(c);
-		np.setMinValue(min);
-		np.setMaxValue(max);
-		np.setValue(prefs.getInt(setting, def));
-		new AlertDialog.Builder(c).setTitle(title).setView(np).setPositiveButton(android.R.string.ok, new OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				np.clearFocus();
-				prefs.edit().putInt(setting, np.getValue()).apply();
-				if (changeTitle)
-					p.setTitle(c.getString(summary, np.getValue()));
-				else
-					p.setSummary(c.getString(summary, np.getValue()));
-			}
-		}).create().show();
-	}
+    static void showNumberPicker(final Context c, final SharedPreferences prefs, final Preference p, final int summary, final int min, final int max, final String title, final String setting, final int def, final boolean changeTitle) {
+        if (c == null) return;
+        final NumberPicker np = new NumberPicker(c);
+        np.setMinValue(min);
+        np.setMaxValue(max);
+        np.setValue(prefs.getInt(setting, def));
+        new AlertDialog.Builder(c).setTitle(title).setView(np)
+                .setPositiveButton(android.R.string.ok, new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        np.clearFocus();
+                        prefs.edit().putInt(setting, np.getValue()).apply();
+                        if (changeTitle) p.setTitle(c.getString(summary, np.getValue()));
+                        else p.setSummary(c.getString(summary, np.getValue()));
+                    }
+                }).create().show();
+    }
 
 }
