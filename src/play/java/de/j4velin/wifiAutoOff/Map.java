e572fb047a7b593eb5ca3eb34d5e216394e45cbc
j4velin <j4velin@gmail.com>
2016-09-16 20:59:13 +0200

    libs updated

--- src/play/java/de/j4velin/wifiAutoOff/Map.java
+++ src/play/java/de/j4velin/wifiAutoOff/Map.java
@@ -16,94 +16,31 @@
 package de.j4velin.wifiAutoOff;
 
 import android.Manifest;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnDismissListener;
 import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.PermissionChecker;
 
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
-public class Map extends FragmentActivity {
+public class Map extends FragmentActivity implements OnMapReadyCallback {
 
     private final static int REQUEST_PERMISSIONS = 1;
 
-    private GoogleMap mMap;
     private GoogleApiClient mGoogleApiClient;
 
     private void showMap() {
         setContentView(R.layout.map);
-
-        LatLng location = getIntent().getParcelableExtra("location");
-
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-
-        if (mMap == null) {
-            if (BuildConfig.DEBUG) Logger.log("Map = null - play services available: " +
-                    GooglePlayServicesUtil.isGooglePlayServicesAvailable(this));
-            Dialog d = GooglePlayServicesUtil
-                    .getErrorDialog(GooglePlayServicesUtil.isGooglePlayServicesAvailable(this),
-                            this, 0);
-            d.setOnDismissListener(new OnDismissListener() {
-                @Override
-                public void onDismiss(final DialogInterface dialog) {
-                    finish();
-                }
-            });
-            d.show();
-            return;
-        }
-
-        if (location == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
-                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                        @Override
-                        public void onConnected(final Bundle bundle) {
-                            Location l = LocationServices.FusedLocationApi
-                                    .getLastLocation(mGoogleApiClient);
-                            if (l != null && mMap.getCameraPosition().zoom <= 2) {
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                        new LatLng(l.getLatitude(), l.getLongitude()), 16));
-                            }
-                            mGoogleApiClient.disconnect();
-                        }
-
-                        @Override
-                        public void onConnectionSuspended(int cause) {
-                            if (BuildConfig.DEBUG) Logger.log("connection suspended: " + cause);
-                        }
-                    }).build();
-
-            mGoogleApiClient.connect();
-
-            mMap.setOnMapClickListener(new OnMapClickListener() {
-                @Override
-                public void onMapClick(final LatLng center) {
-                    getIntent().putExtra("location", center);
-                    setResult(RESULT_OK, getIntent());
-                    finish();
-                }
-            });
-        } else {
-            mMap.addCircle(new CircleOptions().center(location).radius(100).strokeColor(Color.RED)
-                    .fillColor(Color.argb(64, 255, 0, 0)));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
-        }
-
-        mMap.setMyLocationEnabled(true);
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                .getMapAsync(this);
     }
 
     @Override
@@ -124,7 +61,8 @@ public class Map extends FragmentActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, final String[] permissions, final int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, final String[] permissions,
+                                           final int[] grantResults) {
         if (requestCode == REQUEST_PERMISSIONS) {
             if (grantResults[0] == PermissionChecker.PERMISSION_GRANTED &&
                     grantResults[1] == PermissionChecker.PERMISSION_GRANTED) {
@@ -135,5 +73,48 @@ public class Map extends FragmentActivity {
         } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    @Override
+    public void onMapReady(final GoogleMap mMap) {
+        LatLng location = getIntent().getParcelableExtra("location");
+        if (location == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                    .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                        @Override
+                        public void onConnected(final Bundle bundle) {
+                            android.location.Location l = LocationServices.FusedLocationApi
+                                    .getLastLocation(mGoogleApiClient);
+                            if (l != null && mMap.getCameraPosition().zoom <= 2) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(l.getLatitude(), l.getLongitude()), 16));
+                            }
+                            mGoogleApiClient.disconnect();
+                        }
+
+                        @Override
+                        public void onConnectionSuspended(int cause) {
+                            if (BuildConfig.DEBUG) Logger.log("connection suspended: " + cause);
+                        }
+                    }).build();
+
+            mGoogleApiClient.connect();
+
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(final LatLng center) {
+                    getIntent().putExtra("location", center);
+                    setResult(RESULT_OK, getIntent());
+                    finish();
+                }
+            });
+        } else {
+            mMap.addCircle(new CircleOptions().center(location).radius(100).strokeColor(Color.RED)
+                    .fillColor(Color.argb(64, 255, 0, 0)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+        }
+
+        mMap.setMyLocationEnabled(true);
     }
 }
