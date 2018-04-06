21b258118dfb39a9575920f3b56b8fdd900dc194
j4velin <j4velin@gmail.com>
2018-04-06 18:12:28 +0200

    check for power connected on boot

--- src/main/java/de/j4velin/wifiAutoOff/StartReceiver.java
+++ src/main/java/de/j4velin/wifiAutoOff/StartReceiver.java
@@ -19,6 +19,8 @@ package de.j4velin.wifiAutoOff;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 
 /**
  * BroadcastReceiver which receives BOOT_COMPLETE & PACKAGE_REPLACED and then
@@ -35,6 +37,17 @@ public class StartReceiver extends BroadcastReceiver {
                 intent.getDataString().contains(context.getPackageName())) {
             Start.start(context);
             context.startService(new Intent(context, LogDeleteService.class));
+        }
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            // AC already connected on boot?
+            Intent batteryIntent =
+                    context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            int plugged = batteryIntent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+            if (plugged == BatteryManager.BATTERY_PLUGGED_AC ||
+                    plugged == BatteryManager.BATTERY_PLUGGED_USB) {
+                context.sendBroadcast(new Intent(context, Receiver.class)
+                        .setAction(Intent.ACTION_POWER_CONNECTED));
+            }
         }
     }
 
